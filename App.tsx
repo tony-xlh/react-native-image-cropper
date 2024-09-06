e688ce029d2cffe40b093a24501d62b66e450d1b
xulihang <xulihanghai@163.com>
2024-09-06 17:47:40 +0800

    use react native skia to draw the image and the polygon

--- App.tsx
+++ App.tsx
@@ -1,7 +1,8 @@
 import * as React from 'react';
 import { StyleSheet, SafeAreaView, View, Text, Button } from 'react-native';
 import {launchImageLibrary} from 'react-native-image-picker';
-import Cropper from './components/Cropper';
+import Cropper, { Photo } from './components/Cropper';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 const Separator = () => (
   <View style={styles.separator} />
@@ -9,13 +10,19 @@ const Separator = () => (
 
 export default function App() {
   const [showCropper, setShowCropper] = React.useState(false);
-  const [photoUri, setPhotoUri] = React.useState<string|undefined>(undefined);
+  const [photo, setPhoto] = React.useState<Photo|undefined>(undefined);
   const pickAndCrop = async () => {
     const response = await launchImageLibrary({ mediaType: 'photo'});
     if (response && response.assets) {
       if (response.assets[0]!.uri) {
         console.log(response.assets[0]!.uri);
-        setPhotoUri(response.assets[0]!.uri);
+        setPhoto(
+          {
+            photoUri:response.assets[0]!.uri,
+            photoWidth:response.assets[0]!.width as number,
+            photoHeight:response.assets[0]!.height as number,
+          }
+        );
         setShowCropper(true);
       }
     }
@@ -36,7 +43,9 @@ export default function App() {
       )}
       {showCropper && (
         <>
-          <Cropper photoUri={photoUri} onCanceled={()=>setShowCropper(false)}/>
+          <GestureHandlerRootView>
+            <Cropper photo={photo} onCanceled={()=>setShowCropper(false)}/>
+          </GestureHandlerRootView>
         </>
       )}
     </SafeAreaView>
--- components/Cropper.tsx
+++ components/Cropper.tsx
@@ -1,17 +1,33 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import React from 'react';
-import { BackHandler, Image, SafeAreaView, StyleSheet, Text} from 'react-native';
+import React, { useState } from 'react';
+import { BackHandler, useWindowDimensions} from 'react-native';
+import { Canvas, Fill, Image, Points, Rect, useImage, vec  } from '@shopify/react-native-skia';
+import { GestureDetector, Gesture } from 'react-native-gesture-handler';
+import { runOnJS, useSharedValue } from 'react-native-reanimated';
 
+export interface Photo {
+  photoUri:string;
+  photoWidth:number;
+  photoHeight:number;
+}
 
 export interface CropperProps{
-  photoUri:string|undefined;
+  photo?:Photo,
   onCanceled?: () => void;
   onConfirmed?: (base64:string) => void;
 }
+export interface Point {
+  x: number;
+  y: number;
+}
 
 export default function Cropper(props:CropperProps) {
+  const image = useImage(props.photo!.photoUri);
+  const { width, height } = useWindowDimensions();
+  const [points,setPoints] = useState<[Point,Point,Point,Point]>([{x:100,y:50},{x:200,y:50},{x:200,y:100},{x:100,y:100}]);
 
   React.useEffect(() => {
+    console.log(props.photo);
     const backAction = () => {
       if (props.onCanceled) {
         props.onCanceled();
@@ -27,29 +43,49 @@ export default function Cropper(props:CropperProps) {
     return () => backHandler.remove();
   }, []);
 
+  const getPoints = () => {
+    let newPoints = [
+      vec(points[0].x,points[0].y),
+      vec(points[1].x,points[1].y),
+      vec(points[2].x,points[2].y),
+      vec(points[3].x,points[3].y),
+      vec(points[0].x,points[0].y),
+    ];
+    return newPoints;
+  };
+
+  const panGesture = Gesture.Pan()
+    .onChange((e) => {
+      console.log(e);
+      console.log(points);
+      let newPoints:[Point,Point,Point,Point] = points;
+      newPoints[0].x = newPoints[0].x + e.changeX;
+      newPoints[0].y = newPoints[0].y + e.changeY;
+      runOnJS(setPoints)(newPoints);
+    });
+
+  const tapGesture = Gesture.Tap()
+    .onBegin((e) => {
+      console.log(e);
+    });
+
+  const composed = Gesture.Simultaneous(tapGesture, panGesture);
+
   return (
-    <SafeAreaView style={styles.container}>
-      {props.photoUri !== undefined && (
-        <>
-          <Image
-            style={styles.image}
-            source={{uri:props.photoUri}}
-          />
-        </>
-      )}
-      {props.photoUri === undefined && (
-        <Text>No image selected</Text>
-      )}
-    </SafeAreaView>
+    <GestureDetector gesture={composed}>
+      <Canvas style={{ flex: 1 }}>
+        <Fill color="white" />
+        <Image image={image} fit="contain" x={0} y={0} width={width} height={height} />
+        <Points
+          points={getPoints()}
+          mode="polygon"
+          color="lightblue"
+          style="fill"
+          strokeWidth={4}
+        />
+        <Rect style="stroke" strokeWidth={4} x={points[0].x - 10} y={points[0].y - 10} width={10} height={10} color="lightblue" />
+      </Canvas>
+    </GestureDetector>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex:1,
-  },
-  image: {
-    flex:1,
-    resizeMode:'contain',
-  },
-});
