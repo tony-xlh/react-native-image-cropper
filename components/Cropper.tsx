8f6e6b7c0c1c0a8334f53cea995083f5eb24144c
xulihang <xulihanghai@163.com>
2024-09-09 11:25:09 +0800

    use sharedvalue for performance

--- components/Cropper.tsx
+++ components/Cropper.tsx
@@ -1,9 +1,9 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import React, { useMemo, useState } from 'react';
 import { BackHandler, useWindowDimensions} from 'react-native';
-import { Canvas, Fill, Image, Points, Rect, useImage, vec  } from '@shopify/react-native-skia';
+import { Canvas, Fill, Image, Points, Rect, SkPoint, useImage, vec  } from '@shopify/react-native-skia';
 import { GestureDetector, Gesture } from 'react-native-gesture-handler';
-import { runOnJS, useSharedValue } from 'react-native-reanimated';
+import { runOnJS, useDerivedValue, useSharedValue } from 'react-native-reanimated';
 
 export interface Photo {
   photoUri:string;
@@ -24,7 +24,49 @@ export interface Point {
 export default function Cropper(props:CropperProps) {
   const image = useImage(props.photo!.photoUri);
   const { width, height } = useWindowDimensions();
-  const [points,setPoints] = useState<[Point,Point,Point,Point]>([{x:100,y:50},{x:200,y:50},{x:200,y:100},{x:100,y:100}]);
+  const x1 = useSharedValue(100);
+  const y1 = useSharedValue(50);
+  const x2 = useSharedValue(200);
+  const y2 = useSharedValue(50);
+  const x3 = useSharedValue(200);
+  const y3 = useSharedValue(100);
+  const x4 = useSharedValue(100);
+  const y4 = useSharedValue(100);
+  const rectWidth = 10;
+  const rect1X = useDerivedValue(() => {
+    return x1.value - rectWidth;
+  },[x1]);
+  const rect1Y = useDerivedValue(() => {
+    return y1.value - rectWidth;
+  },[y1]);
+  const rect2X = useDerivedValue(() => {
+    return x2.value;
+  },[x2]);
+  const rect2Y = useDerivedValue(() => {
+    return y2.value - rectWidth;
+  },[y2]);
+  const rect3X = useDerivedValue(() => {
+    return x3.value;
+  },[x3]);
+  const rect3Y = useDerivedValue(() => {
+    return y3.value;
+  },[y3]);
+  const rect4X = useDerivedValue(() => {
+    return x4.value - rectWidth;
+  },[x4]);
+  const rect4Y = useDerivedValue(() => {
+    return y4.value;
+  },[y4]);
+  const points = useDerivedValue(() => {
+    let newPoints = [
+      vec(x1.value,y1.value),
+      vec(x2.value,y2.value),
+      vec(x3.value,y3.value),
+      vec(x4.value,y4.value),
+      vec(x1.value,y1.value),
+    ];
+    return newPoints;
+  },[x1,y1,x2,y2,x3,y3,x4,y4]);
 
   React.useEffect(() => {
     console.log(props.photo);
@@ -43,28 +85,12 @@ export default function Cropper(props:CropperProps) {
     return () => backHandler.remove();
   }, []);
 
-  const calculatePoints = () => {
-    let newPoints = [
-      vec(points[0].x,points[0].y),
-      vec(points[1].x,points[1].y),
-      vec(points[2].x,points[2].y),
-      vec(points[3].x,points[3].y),
-      vec(points[0].x,points[0].y),
-    ];
-    return newPoints;
-  };
-
-  const getPoints = useMemo(
-    () => calculatePoints(),
-    [points]
-  );
 
   const panGesture = Gesture.Pan()
     .onChange((e) => {
-      let newPoints:[Point,Point,Point,Point] = points;
-      newPoints[0].x = newPoints[0].x + e.changeX;
-      newPoints[0].y = newPoints[0].y + e.changeY;
-      runOnJS(setPoints)(newPoints);
+      console.log(e);
+      x1.value = x1.value + e.changeX;
+      y1.value = y1.value + e.changeY;
     });
 
   const tapGesture = Gesture.Tap()
@@ -74,19 +100,27 @@ export default function Cropper(props:CropperProps) {
 
   const composed = Gesture.Simultaneous(tapGesture, panGesture);
 
+  const rects = () => {
+    let rectList = [{x:rect1X,y:rect1Y},{x:rect2X,y:rect2Y},{x:rect3X,y:rect3Y},{x:rect4X,y:rect4Y}];
+    const items = rectList.map((rect,index) =>
+      <Rect key={'rect-' + index}  style="stroke" strokeWidth={4} x={rect.x} y={rect.y} width={rectWidth} height={rectWidth} color="lightblue" />
+    );
+    return items;
+  };
+
   return (
     <GestureDetector gesture={composed}>
       <Canvas style={{ flex: 1 }}>
         <Fill color="white" />
         <Image image={image} fit="contain" x={0} y={0} width={width} height={height} />
         <Points
-          points={getPoints}
+          points={points}
           mode="polygon"
           color="lightblue"
           style="fill"
           strokeWidth={4}
         />
-        <Rect style="stroke" strokeWidth={4} x={points[0].x - 10} y={points[0].y - 10} width={10} height={10} color="lightblue" />
+        {rects()}
       </Canvas>
     </GestureDetector>
   );
